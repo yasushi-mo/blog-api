f68992af63156dca9486fa1d77a8cf24bdd21e5d
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-01-28 15:35:26 +0900

    Add:crypto

---
+++ crypto.ts
@@ -0,0 +1,25 @@
+import {
+  createCipheriv,
+  createDecipheriv,
+  randomBytes,
+  scryptSync,
+} from "crypto";
+
+const algorithm = "aes-192-cbc";
+const salt = "12345678";
+const password = "password";
+const key = scryptSync(password, salt, 24);
+const iv = randomBytes(16);
+
+const data = "plain text";
+const inputEncoding = "utf8";
+const outputEncoding = "hex";
+
+const cipher = createCipheriv(algorithm, key, iv);
+const decipher = createDecipheriv(algorithm, key, iv);
+
+const encrypted = cipher.update(data, inputEncoding, outputEncoding);
+export const encryptedText = encrypted + cipher.final(outputEncoding);
+
+const decrypted = decipher.update(encryptedText, outputEncoding, inputEncoding);
+export const decryptedText = decrypted + decipher.final(inputEncoding);
